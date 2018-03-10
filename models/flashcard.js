1835736bac9606c699423f5b6daddad0d2bb0615
David Cheng <dkcheng10@gmail.com>
2018-03-10 12:54:55 -0800

    comment changes

--- models/flashcard.js
+++ models/flashcard.js
@@ -2,15 +2,14 @@ const User = require('./user.js');
 
 module.exports = function (sequelize, DataTypes) {
     var Flashcard = sequelize.define("Flashcard", {
-        // Giving the Author model a name of type STRING
+        // Giving the User model a name of type STRING
         Category: DataTypes.STRING,
-        //sets can be an array instead of strings
         Title: DataTypes.STRING,
         Body: DataTypes.STRING,
     });
     Flashcard.associate = function (models) {
-        // We're saying that a Post should belong to an Author
-        // A Post can't be created without an Author due to the foreign key constraint
+        // We're saying that a Flashcard should belong to an User
+        // A Flashcardcan't be created without an User due to the foreign key constraint
         Flashcard.belongsTo(models.User, {
             foreignKey: {
                 allowNull: false
@@ -20,4 +19,3 @@ module.exports = function (sequelize, DataTypes) {
     return Flashcard;
 };
 
-// Sets.hasMany(Flashcards);
