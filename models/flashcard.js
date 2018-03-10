91303612c698a6872b6b5bf9527b24d516b41a93
David Cheng <dkcheng10@gmail.com>
2018-03-10 10:24:10 -0800

    changed seqalized. One DB 2 tables

--- models/flashcard.js
+++ models/flashcard.js
@@ -1,20 +1,20 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('assoc', 'root', '', {
+const sequelize = new Sequelize('flashcard_db', 'root', '', {
     dialect: 'mysql',
 });
 
-const Sets = sequelize.define('Set', {
-    name: Sequelize.STRING
-})
+//Solution: combine into one model 
 
-Sets.hasMany(Flashcards);
 module.exports = function (sequelize, DataTypes) {
     var Flashcard = sequelize.define("FlashCard", {
         // Giving the Author model a name of type STRING
         Sets: DataTypes.String,
+        //sets can be an array instead of strings
         Title: DataTypes.STRING,
         Body: DataTypes.STRING,
     });
 
     return Flashcard;
 };
+
+Sets.hasMany(Flashcards);
