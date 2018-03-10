baee3444457aa95c591b6c80c14aaed452ba8397
David Cheng <dkcheng10@gmail.com>
2018-03-10 10:32:54 -0800

    user db

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 ### Schema
 
--- CREATE DATABASE users_db;
--- USE users_db;
+CREATE DATABASE users_db;
+USE users_db;
 
-CREATE DATABASE flashcards_db;
-USE flashcards_db;
+-- CREATE DATABASE flashcards_db;
+-- USE flashcards_db;
 
 -- Only one database is needed to reference user and flashcards
